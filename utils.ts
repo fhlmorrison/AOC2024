61e273ecce845686b7ad2d0dd699c710db4d9e75
Fraser Morrison <frazemor@hotmail.com>
2024-12-12 03:36:54 -0500

    Day 12

--- utils.ts
+++ utils.ts
@@ -184,14 +184,14 @@ export function windows<T>(arr: T[], size: number): T[][] {
  * @example DIRECTIONS_MAP["E"] -> [0, 1]
  */
 export const DIRECTIONS_MAP = {
-  N: [-1, 0],
-  E: [0, 1],
-  S: [1, 0],
-  W: [0, -1],
+  N: [0, -1],
+  E: [1, 0],
+  S: [0, 1],
+  W: [-1, 0],
   NW: [-1, -1],
-  NE: [-1, 1],
+  NE: [1, -1],
   SE: [1, 1],
-  SW: [1, -1],
+  SW: [-1, 1],
 };
 
 /**
